4431d64fcc9e3402ab8322fc415711622631154f
florianscherl <florianscherl@live.at>
2022-06-20 11:25:45 +0200

    init

---
+++ src/test/java/com/example/randomnumberdemo/RandomNumberDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.randomnumberdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomNumberDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
