ab1c1168f898c7ef86de46df2d83c4774b573e45
florianscherl <florianscherl@live.at>
2022-06-20 12:03:47 +0200

    develop init

--- src/main/java/com/example/randomnumberdemo/RandomNumberDemoApplication.java
+++ src/main/java/com/example/randomnumberdemo/RandomNumberDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RandomNumberDemoApplication {
-
+//test
     public static void main(String[] args) {
         SpringApplication.run(RandomNumberDemoApplication.class, args);
     }
